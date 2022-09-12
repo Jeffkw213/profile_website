7abf445917dc489d88f27f91a3a0153a32a1426f
Jeff Kwan <jeffkwan10@gmail.com>
2022-09-12 15:55:49 -0400

    This is the starting point of my Profile. I am doing this so I can get a better understanding of how to code on react.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const list = [
+  {
+    title: 'React',
+    url: 'https://www.youtube.com/?bp=wgUCEAE%3D',
+    author: 'Jeff Kwan',
+    num_comments: 3,
+    points: 4, 
+    objectID: 0,
+  },
+  {
+    title: 'WTF',
+    url: 'https://www.linkedin.com/jobs/collections/recommended/?currentJobId=3194734192',
+    author: 'Who dafuq',
+    num_comments: 5,
+    points: 6,
+    objectID: 7,
+  }
+]
+//components for list
+const List = () => {
+  return(
+    <ul>
+      {list.map(function(item){
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+// component for search 
+const Search = () => {
+  return(
+    <div>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type="text"/>
     </div>
+  );
+}
+
+const App = () => {
+  
+  // instances
+  // const greeting = {
+  //   hi: 'Hello',
+  //   title: 'React',
+  // }; 
+  
+  // Function
+  // function getTitle(title){
+  //   return title;
+  // }
+  
+  //Creating a list
+  return (
+   <div>
+      <h1>
+        Hello React
+      </h1>
+      <hr/>
+      <List/>
+      <Search/>
+      {/* <label htmlFor='search'>Search</label>
+      <input id='search' type='text' /> */}
+   </div>
   );
 }
 
