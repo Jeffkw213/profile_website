f26ab333e88f077fa55b9ba770396c0b8fa4841e
Jeff Kwan <jeffkwan10@gmail.com>
2022-10-26 17:16:46 -0400

    Refactoring some code from Title and worked on the Work Experience page

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,51 +4,59 @@
 import { FaProjectDiagram } from 'react-icons/fa';
 import { BiArrowToBottom } from 'react-icons/bi';
 export default function Title({ Name, Description }) {
-
   function Stars() {
-    const arr = [];    
+    const arr = [];
     for (let i = 0; i < 100; i++) {
       var vw = 111; //getting the width of the window
       var vh = 200; //getting the height of the window
-      arr.push(<div className={`bg-white w-2 h-2 rounded-full absolute top-[${vh}px] left-[${vw}px]`} key={`${i}`}></div>);
+      arr.push(
+        <div className={`bg-white w-2 h-2 rounded-full absolute top-[${vh}px] left-[${vw}px]`} key={`${i}`}></div>
+      );
     }
     return arr;
   }
+
+  const BackGround = () => (
+    <div className="absolute inset-0.5 bg-gradient-to-r from-red-600 to-blue-600 rounded-full blur-2xl bg-opacity-80"></div>
+  );
+  const SomeButtons = () => (
+    <div className="flex items-center justify-center py-2">
+      <a href="#project" className="px-2">
+        <button className="flex items-center justify-center p-2 px-3 bg-transparent text-white transition delay-150 duration-300 ease-in-out hover:scale-110 shadow-2xl">
+          <FaProjectDiagram className="text-white mr-2" /> Projects
+        </button>
+      </a>
+      <a href="/Resume.pdf" target="_blank" className="px-2">
+        <button className="flex items-center justify-center p-2 px-3 bg-transparent border-2 border-indigo-100 text-white transition delay-150 duration-300 ease-in-out hover:rotate-y-360">
+          <BiArrowToBottom className=" text-white mr-2" />
+          Resume
+        </button>
+      </a>
+    </div>
+  );
+  const About = () => (
+    <div>
+      <h1 className="font-Nexa text-4xl sm:text-7xl text-shadow-md text-center pt-4 pb-2">{Name}</h1>
+      <p className="p-4 mx-0 sm:mx-14 max-w-screen-lg text-shadow-md min-w-[50%]">{Description}</p>
+    </div>
+  );
+  const Body = () => (
+    <div className="relative max-w-[880px]">
+      <BackGround />
+      <div className="relative m-2 py-12 text-white ">
+        <About />
+        {/* Buttons */}
+        <SomeButtons />
+      </div>
+    </div>
+  );
   return (
     <div className="bg-black max-w-1/2 w-full px-4" id="home">
       <div>
         <Stars />
       </div>
       <div className="flex items-center justify-center h-screen">
-        <div className="relative max-w-[880px]">
-          <div className="absolute inset-0.5 bg-gradient-to-r from-red-600 to-blue-600 rounded-full blur-2xl bg-opacity-80"></div>
-
-          <div className="relative m-2 py-12 text-white ">
-            <h1 className="font-Nexa text-4xl sm:text-7xl text-shadow-md text-center pt-4 pb-2">{Name}</h1>
-            <p className="p-4 mx-0 sm:mx-14 max-w-screen-lg text-shadow-md min-w-[50%]">{Description}</p>
-
-            {/* Buttons */}
-            <div className="flex items-center justify-center py-2">
-              <a href="#project" className="px-2">
-                <button
-                  className="flex items-center justify-center p-2 px-3 bg-transparent text-white 
-        transition delay-150 duration-300 ease-in-out hover:scale-110 shadow-2xl"
-                >
-                  <FaProjectDiagram className="text-white mr-2" /> Projects
-                </button>
-              </a>
-              <a href="/Resume.pdf" target="_blank" className="px-2">
-                <button
-                  className="flex items-center justify-center p-2 px-3 bg-transparent border-2 border-indigo-100 text-white 
-        transition delay-150 duration-300 ease-in-out hover:rotate-y-360"
-                >
-                  <BiArrowToBottom className=" text-white mr-2" />
-                  Resume
-                </button>
-              </a>
-            </div>
-          </div>
-        </div>
+        <Body />
       </div>
     </div>
   );
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,21 +1,29 @@
 import { HiChevronDoubleDown } from 'react-icons/hi';
 export default function Work({ employ }) {
   return (
-    <div className="h-screen max-w-1/2 flex flex-col p-2" id="work">
-      <h1 className="font-Nexa text-4xl sm:text-7xl text-center pt-4 pb-2"> Work Experience </h1>
-      {employ.map((item, index) => {
-        return (
-          <div key={index} className="">
-            <p>{item.Role}</p>
-            <p>{item.Description}</p>
-            <p>{item.Date}</p>
-          </div>
-        );
-      })}
-      
-      <a href="#project" className='flex justify-center items-center'>
-        <HiChevronDoubleDown className="text-white m-32 bg-black mt-auto h-10 w-10 rounded-full animate-bounce flex justify-start items-center" />
-      </a>
+    <div className="h-screen w-full max-w-1/2 bg-black">
+      <div className="flex items-center justify-center " id="work">
+        <div className="bg-violet-700">
+          <h2 className="font-Nexa text-4xl sm:text-7xl text-center font-bold pt-4 pb-2 text-white">
+            {' '}
+            Work Experience{' '}
+          </h2>
+          {employ.map((item, index) => {
+            return (
+              <div key={index} className="text-white border-2 border-white w-132 m-4">
+                <p className="">{item.Role}</p>
+                <p className="w-128">{item.Description}</p>
+                <p>{item.Date}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <a href="#project" className="text-center items-center">
+          <HiChevronDoubleDown className="text-white bg-red-700 mt-auto h-10 w-10 rounded-full animate-bounce flex justify-start items-center" />
+        </a>
+      </div>
     </div>
   );
 }
